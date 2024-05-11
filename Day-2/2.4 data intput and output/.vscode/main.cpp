03c0797cf60ef3ce97920242584cfa15ca90ef47
surya_chilukuri <tejadst202@gmail.com>
2024-05-11 10:41:36 +0200

    Day2 of learning C++ End day commit

--- Day-2/2.4 data intput and output/.vscode/main.cpp
+++ Day-2/2.4 data intput and output/.vscode/main.cpp
@@ -13,6 +13,14 @@ int main()
 
     //taking data from termial
 
+    int age;
+    std::string name;
+    std::cout << "Please enter your name and age " << std::endl;
+    std::getline(std::cin,name);
+    std::cin >> age;
+    std::cout << "You are  " << name << " and " << age << "years old !" << std::endl;
+
+
     
 
     return 0;
